b5b4ad7d73e274f0dc626c29c57d67c1eb9919bb
HP <HP@LAPTOP-PJID2E0G>
2024-07-26 16:07:48 +0530

    this is my java programs

---
+++ CoreJavaExample/src/Day0/AdditionDemo.java
@@ -0,0 +1,12 @@
+package Day0;
+
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TNS....");
+		System.out.print("by Praveena");
+	
+
+	}
+
+}
---
+++ CoreJavaExample/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module CoreJavaExample {
+}
